27e174b7b31aa0b4e415ac4cb20e4b6ad5b3605f
Mister Coder <3dprintingwebstienl@gmail.com>
2022-06-23 11:44:36 +0200

    aftelklok

---
+++ CountDown/aftel.js
@@ -0,0 +1,36 @@
+console.log("works");
+
+const p = document.querySelector("p");
+console.log(p);
+const begin = Date.UTC(2022, 7, 1);
+console.log(begin);
+timeShow();
+setInterval(timeShow, 100);
+
+function timeShow() {
+  const now = Date.now();
+  const diff = begin - now;
+  const qw = TransForm(diff);
+  const str = `nog ${qw.dagen} dagen en ${qw.uur} uur en ${qw.minuten} minuten en ${qw.seconds} seconden`;
+  p.innerText = str;
+}
+
+function TransForm(timeNumber) {
+  let werkelijk = Math.floor(timeNumber / 1000);
+  const dagen = Math.floor(werkelijk / (60 * 60 * 24));
+  werkelijk = werkelijk % (dagen * 60 * 60 * 24);
+  const uur = Math.floor(werkelijk / (60 * 60));
+  werkelijk = werkelijk % (uur * (60 * 60));
+  const minuten = Math.floor(werkelijk / 60);
+  werkelijk = werkelijk % (minuten * 60);
+  const seconds = Math.floor(werkelijk);
+
+  const ob = {
+    seconds: seconds,
+    minuten: minuten,
+    uur: uur,
+    dagen: dagen,
+  };
+
+  return ob;
+}

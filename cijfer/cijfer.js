430db4bfd494ff3a233552ba2db33f80840c617a
Mister Coder <3dprintingwebstienl@gmail.com>
2022-11-03 22:38:40 +0100

    het werkt nu

--- cijfer/cijfer.js
+++ cijfer/cijfer.js
@@ -1,3 +1,27 @@
 function cijfer(points = 100, maxPoints = 100, gokkans = 0, hallmark = 55) {
-  return ((points - gokkans) / (maxPoints - gokkans)) * (hallmark / 55);
+  return ((points - gokkans) / (maxPoints - gokkans)) * (hallmark / 50);
+}
+
+class CijferBerekenen {
+  constructor({ points, maxPoints, gokkans = 0, hallmark = 55 }) {
+    this.punten = points;
+    this.maxPoints = maxPoints;
+    this.gokkans = gokkans;
+    this.hallmark = hallmark;
+  }
+  cijfer() {
+    return (
+      ((this.punten - this.gokkans) / (this.maxPoints - this.gokkans)) *
+      (this.hallmark / 50)
+    );
+  }
+  canvasCors({ height, width }) {
+    const w = width;
+    const h = height;
+    const ans = {};
+    ans.x = (this.punten / this.maxPoints) * w;
+    const cijf = this.cijfer();
+    ans.y = h -h * (cijf);
+    return ans;
+  }
 }
--- cijfer/index.js
+++ cijfer/index.js
@@ -1,3 +1,5 @@
+window.onload = main;
+
 const gokkans = document.querySelector("#gokkans");
 const points = document.querySelector("#points");
 const maxPoints = document.querySelector("#maxPoints");
@@ -5,16 +7,10 @@ const hallmark = document.querySelector("#hallmark");
 
 // console.log(gokkans);
 // console.log(gokkans,points,maxPoints,hallmark);
-let mp, currentpoint, theHallMark, geusscorrect;
-console.debug(gokkans);
-console.info(hallmark);
 
-window.onload = main;
 
-let px = 10;
-let py = 10;
-const sx = 20;
-const sy = 20;
+let mp,  theHallMark, geusscorrect;
+
 const canvv1 = document.querySelector("canvas#main");
 const canvv2 = document.querySelector("canvas#extra");
 
@@ -24,10 +20,10 @@ const width = canvv1.width;
 canvv2.height = height;
 canvv2.width = width;
 
-const can = canvv1.getContext("2d");
 const canExtra = canvv2.getContext("2d");
+const pop = canvv1.getContext("2d");
 
-const blockk = height / sx;
+
 
 console.log(canvv2);
 
@@ -35,25 +31,57 @@ console.log(canvv2);
 canExtra.textAlign = "center";
 canExtra.font = "100px sans-serif";
 console.log(maxPoints);
-function Draw() {
-  requestAnimationFrame(Draw);
-  const pop = canvv1.getContext("2d");
-  pop.beginPath();
+const ActualVal = new CijferBerekenen({points:0,maxPoints: mp,gokkans: geusscorrect, hallmark:theHallMark});
+function Draw(cans = canExtra, { height, width }) {
+  let b = ActualVal.cijfer();
+  cans.clearRect(0, 0, canvv1.width, canvv1.height);
+  cans.fillText(b, width * 0.5, height * 0.5, height);
+}
 
+function DrawGraph( canv = canvv1 , {}) {
+  const pop = canvv1.getContext("2d");
+  const r = [];
+  for (let i = 0; i < mp; i += 0.25) {
+    const inf = {
+      maxPoints: mp,
+      points: i,
+      gokkans: geusscorrect,
+      hallmark: theHallMark,
+    };
+    const cijf = new CijferBerekenen(inf);
+    r.push(cijf);
+  }
+  pop.clearRect(0, 0, canvv1.width, canvv2.height);
+  pop.beginPath();
+  for (const Cijfer of r) {
+    const t = Cijfer.canvasCors(canvv1);
+    pop.lineTo(t.x, t.y);
+  }
+  pop.stroke();
   pop.closePath();
-  // art.fillRect(40,40,300,300);
+}
+
+function UpdateVar() {
   mp = Number(maxPoints.value);
   geusscorrect = Number(gokkans.value);
   currentpoint = Number(points.value);
   theHallMark = Number(hallmark.value);
-  let b = cijfer(currentpoint, mp, geusscorrect, theHallMark);
-canExtra.clearRect(0,0,canvv1.width,canvv1.height)
-  canExtra.fillText(b, 200, 200, 400);
+  gokkans.max = points.max = maxPoints.value;
+  ActualVal.punten = currentpoint;
+  ActualVal.maxPoints = mp;
+  ActualVal.gokkans = geusscorrect;
+  ActualVal.hallmark= theHallMark
+  DrawGraph(canvv2, {});
+  Draw(canExtra, canvv2);
 }
-
 function main() {
   console.log("succes");
-  Draw();
+  UpdateVar();
+
   return;
 }
 
+for (const inputt of document.querySelectorAll("input")) {
+  console.log(inputt);
+  inputt.onchange = UpdateVar
+}
